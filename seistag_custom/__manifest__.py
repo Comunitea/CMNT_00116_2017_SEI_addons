05a9c5119147fd2f87c5da706278979ed545b220
Roodin <santi@comunitea.com>
2019-01-08 21:20:23 +0100

    [ADD] sestag_custom. Correcciones

--- seistag_custom/__manifest__.py
+++ seistag_custom/__manifest__.py
@@ -2,7 +2,7 @@
 # © 2017 Comunitea
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
-    'name': 'Custom modification for Seistaf    ',
+    'name': 'Custom modification for Seistag',
     'summary': '',
     'version': '11.0.1.0.0',
     'category': 'Uncategorized',
@@ -17,7 +17,7 @@
     'data': [
         'views/harbour.xml',
         'views/sale_view.xml',
-        'views/purchase_view.xml'
+        'views/purchase_view.xml',
         'security/ir.model.access.csv'
     ],
 }
