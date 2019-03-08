13c98afd1a42ad57ea9bb7fe92bf1983c40ee3e9
mariavaal <vazquezalonso.maria@gmail.com>
2019-03-08 13:37:14 +0100

    [FIX]custom_documents: ajustes en factura

--- custom_documents/models/account_invoice.py
+++ custom_documents/models/account_invoice.py
@@ -1,15 +1,23 @@
 # © 2018 Comunitea
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models, fields, api, _
+from odoo import models, fields, api
 
 
 class AccountInvoice(models.Model):
     _inherit = "account.invoice"
 
+    #Deposito / anticipo
+    advance = fields.Float(string="Advance")
+    balance_outstanding = fields.Float(compute = '_calculate_balance_outstanding', string="Balance outstanding")
 
     incoterm = fields.Many2one(
         'stock.incoterms', 'Shipment Terms',
         help="International Commercial Terms are a series of predefined commercial terms used in international transactions.")
     port_destination=fields.Text(string= "Port destination")
     unit_type_id = fields.Many2one('sale.order.unit.type', 'Unit types')
+
+
+    @api.depends('advance','balance_outstanding')
+    def _calculate_balance_outstanding(self):
+        self.balance_outstanding=self.amount_untaxed - self.advance
